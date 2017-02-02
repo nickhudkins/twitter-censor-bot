2edca9d5571ab097f6d63ddedb9834fa72e9228d
Nick Hudkins <nick@nickhudkins.com>
2017-02-01 22:12:03 -0500

    improved logging and deployment

--- logger.js
+++ logger.js
@@ -3,7 +3,8 @@ require('winston-papertrail').Papertrail;
 
 const winstonPapertrail = new winston.transports.Papertrail({
   host: process.env.LOGGING_URL,
-  port: process.env.LOGGING_PORT
+  port: process.env.LOGGING_PORT,
+  hostname: process.env.LOGGING_HOST_NAME,
 })
 
 const logger = new winston.Logger({
